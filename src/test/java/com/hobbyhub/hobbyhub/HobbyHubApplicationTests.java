2b3fb500720536041ee4609d9136940aa13fbcf2
Gitika Kalal <gitikakalal21@gmail.com>
2025-07-23 10:41:21 -0700

    Initial commit: working /users route and PostgreSQL connected

---
+++ src/test/java/com/hobbyhub/hobbyhub/HobbyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyhub.hobbyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
